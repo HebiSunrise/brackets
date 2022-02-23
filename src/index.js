9d1d224d4d87bcab7a3400c38a451b78c9ceb676
HebiSunrise <93012812+HebiSunrise@users.noreply.github.com>
2022-02-23 19:52:32 +0300

    add: my first solutin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) return false
+  const arr = new Map(bracketsConfig);
+  let res = true
+  let subStr = str[0]
+  let i = 0, r = 1
+
+  while (res){
+      if (i < 0){
+          subStr += str[r] 
+          i=0
+          r++
+      }
+      if(arr.get(subStr[i]) == str[r]){
+          subStr = subStr.slice(0, i)
+          r++
+          i--
+      }
+      else {
+         subStr += str[r] 
+         i++
+         r++
+      } 
+      if (r == str.length && subStr == "") return true
+      if (r == str.length && subStr != "") return false
+  }
 }
